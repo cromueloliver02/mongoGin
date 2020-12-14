4a2d317009f2f597944b1f171e8a2c9be645860c
Cromuel Barut <cromuelbarut@gmail.com>
2020-12-14 14:28:00 +0800

    polished not found page

--- _client/src/components/pages/NotFound.js
+++ _client/src/components/pages/NotFound.js
@@ -7,12 +7,12 @@ const NotFound = ({ isAuthenticated }) => {
 	return (
 		<section className='about'>
 			<div className='container'>
-				<h2>
+				<h2 className='mb-4'>
 					<i className='fab fa-envira'></i> MONGOGIN
 				</h2>
-				<h3 className='text-center'>404: Page Not Found</h3>
+				<h3 className='text-center mb-4'>404: Page Not Found</h3>
 				<Link
-					className='btn'
+					className='btn text-center d-block m-auto btn-notFound'
 					to={`${isAuthenticated ? '/dashboard' : '/'}`}
 				>
 					Go back
